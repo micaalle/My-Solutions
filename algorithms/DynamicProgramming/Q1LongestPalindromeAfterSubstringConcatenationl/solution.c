601a2b01eddea08d7bca4e1ef547893828af4004
Michael <mbiskup2608@gmail.com>
2025-03-31 19:44:31 -0400

    Update solution.c

--- algorithms/DynamicProgramming/Q1LongestPalindromeAfterSubstringConcatenationl/solution.c
+++ algorithms/DynamicProgramming/Q1LongestPalindromeAfterSubstringConcatenationl/solution.c
@@ -1,7 +1,7 @@
  bool isPalindrome(char *str) {
     int len = strlen(str);
     for (int i = 0; i < len / 2; i++) {
-        
+         
         if (str[i] != str[len - 1 - i]) {
             return false;
         }
