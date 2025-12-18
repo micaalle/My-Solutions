ee51877ef1ed65b053c8163e076f57c105b62f45
Michael <mbiskup2608@gmail.com>
2025-12-18 14:46:05 -0500

    Create solution2.cpp

---
+++ algorithms/easyProblems/2579CountTotalNumberOfColoredCells/solution2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long coloredCells(int n) {
+        long long count = 0, inc = 1;
+        if (n == 1)
+            return 1;
+        for (int i = n; i > 1; i--) {
+            count = count + 4 * inc;
+            n--;
+            inc++;
+        }
+        return count + 1;
+    }
+};
