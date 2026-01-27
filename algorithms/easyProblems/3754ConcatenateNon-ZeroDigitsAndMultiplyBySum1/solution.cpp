d5358f3c182c750d9b37b1b861fef61759dbc4e2
Michael <mbiskup2608@gmail.com>
2026-01-27 15:05:48 -0500

    Create solution.cpp

---
+++ algorithms/easyProblems/3754ConcatenateNon-ZeroDigitsAndMultiplyBySum1/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long sumAndMultiply(int n) {
+        long long ret = 0;
+        long long x = 0;
+
+        string tmp = to_string(n);
+        int cnt = 0;
+        for(int i = tmp.length() - 1; i >= 0; i--){
+            if(tmp[i] != '0'){
+                ret += tmp[i] - '0';
+                x += (tmp[i] - '0') * pow(10, cnt);
+                cnt++;
+            }
+
+        }
+        return ret * x;
+    }
+};
