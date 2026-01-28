70a0badff7ac93cb0d77785e1c5bc22112ef2b41
Michael <mbiskup2608@gmail.com>
2026-01-28 12:31:01 -0500

    Create solution.cpp

---
+++ algorithms/easyProblems/3668RestoreFinishingOrder/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
+        unordered_set<int> friendSet(friends.begin(), friends.end());
+        vector<int> ret;
+        for (int i = 0; i < order.size(); i++) {
+            if (friendSet.count(order[i])) {
+                ret.push_back(order[i]);
+            }
+        }
+        return ret;
+    }
+};
