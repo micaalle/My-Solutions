ef44387c59b38b3c6b2ff098738566ba2a75f5b0
Michael <mbiskup2608@gmail.com>
2026-01-28 12:42:48 -0500

    Create solution.cpp

---
+++ algorithms/easyProblems/3658GDOfOddAndEvenSums/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        int odd = 0, even = 0;
+        
+        for(int i = 1; i <= n * 2; i++){
+            if(i % 2 == 0) even += i;
+            else odd += i;
+        }
+
+        int tmp = min(even, odd);
+        while((even % tmp != 0) || (odd % tmp != 0)){
+            tmp--;
+        }
+        return tmp;
+    }
+};
