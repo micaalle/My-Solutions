724d308f74c675d534c19930556d1e4b844c5988
Michael <mbiskup2608@gmail.com>
2026-01-30 10:48:00 -0500

    Create solution.cpp

---
+++ algorithms/easyProblems/2235/solution.cpp
@@ -0,0 +1,8 @@
+//Its probably a little ridiculous to upload this to my solutions but ive put all the other ones so I cant stop here
+
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+};
