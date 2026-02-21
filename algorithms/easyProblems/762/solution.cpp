708628d136e3119524e0fa4b8f301f2c1afd5e5e
Michael <mbiskup2608@gmail.com>
2026-02-21 15:05:24 -0500

    Create solution.cpp

---
+++ algorithms/easyProblems/762/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    static int countPrimeSetBits(int left, int right) {
+        int prime[]={2, 3, 5, 7, 11, 13, 17, 19};
+        bitset<21> isPrime=0;
+        for(int p: prime) isPrime[p]=1;
+        int ret=0;
+        for(unsigned i=left; i<=right; i++){
+            int b=popcount(i);
+            if (isPrime[b]) ret++;
+        }
+        return ret;
+    }
+};
